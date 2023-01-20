9e93e177ebc4d1aa872259ffed8d6cbd66129c24
Kyle_Home <trong.nguyen@student.vinci.com>
2023-01-20 11:00:09 +0100

    creat check char number

--- src/services/validations.js
+++ src/services/validations.js
@@ -3,7 +3,10 @@
 const isEmpty = (label) => !label || label.length === 0;
 
 const checkGamerTag = (label) => {
-    return false;
+    if (label.length < 8) {
+        return false;
+    }
+    return true;
 };
 
 module.exports = {
